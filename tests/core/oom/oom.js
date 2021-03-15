e34c4030cf695cb437cecf4a91573bafb33eb5ae
rahulj <rahulj@jfrog.com>
2021-03-15 09:45:10 +0530

    PIPE-5000 Added pipeline source

---
+++ tests/core/oom/oom.js
@@ -0,0 +1,15 @@
+'use strict';
+const list = [];
+setInterval(()=> {
+  const record = new MyRecord();
+  list.push(record);
+},10);
+function MyRecord() {
+  var x='hii';
+  this.name = x.repeat(10000000);
+  this.id = x.repeat(10000000);
+  this.account = x.repeat(10000000);
+}
+setInterval(()=> {
+  console.log(process.memoryUsage())
+},10000);
